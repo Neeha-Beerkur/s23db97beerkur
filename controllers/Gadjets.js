0f340cd2d9585976ed2c3e0254b06b977449aa4d
Neeha-Beerkur <123040403+Neeha-Beerkur@users.noreply.github.com>
2023-04-18 20:09:32 -0500

    Added

--- controllers/Gadjets.js
+++ controllers/Gadjets.js
@@ -32,10 +32,19 @@ document.Gadjet_Type = req.body.Gadjet_Type;
  }
 };
 
-// Handle Gadjets delete form on DELETE.
-exports.Gadjets_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Gadjets delete DELETE ' + req.params.id);
+// Handle Gadjets delete on DELETE.
+exports.Gadjets_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Gadjets.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
 };
+
 // Handle Gadjets update form on PUT.
 exports.Gadjets_update_put = async function(req, res) {
  console.log(`update on id ${req.params.id} with body
@@ -43,8 +52,8 @@ ${JSON.stringify(req.body)}`)
  try {
  let toUpdate = await Gadjets.findById( req.params.id)
  // Do updates of properties
- if(req.body.Gadjets_type)
- toUpdate.Gadjet_type = req.body.Gadjet_type;
+ if(req.body.Gadjets_Type)
+ toUpdate.Gadjets_Type = req.body.Gadjets_Type;
  if(req.body.Gadjet_Name) toUpdate.Gadjet_Name = req.body.Gadjet_Name;
  if(req.body.Gadjet_Price) toUpdate.Gadjet_Price = req.body.Gadjet_Price;
  let result = await toUpdate.save();
@@ -70,6 +79,20 @@ exports.Gadjets_view_all_Page = async function(req, res) {
     } 
 };
 
+// Handle a show one view with id specified by query
+exports.Gadjets_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Gadjets.findById( req.query.id)
+    res.render('Gadjetsdetail',
+   { title: 'Gadjets Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
 // for a specific Costume.
 exports.Gadjets_detail = async function(req, res) {
     console.log("detail" + req.params.id)
@@ -79,5 +102,47 @@ exports.Gadjets_detail = async function(req, res) {
     } catch (error) {
     res.status(500)
     res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Gadjets_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Gadjetscreate', { title: 'Gadjets Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.Gadjets_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Gadjets.findById(req.query.id)
+    res.render('Gadjetsupdate', { title: 'Gadjets Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+  };
+
+  // Handle a delete one view with id from query
+exports.Gadjets_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Gadjets.findById(req.query.id)
+    res.render('Gadjetsdelete', { title: 'Gadjets Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
    };
--- routes/Gadjets.js
+++ routes/Gadjets.js
@@ -4,4 +4,16 @@ var router = express.Router();
 /* GET Gadjets */
 router.get('/', Gadjets_controlers.Gadjets_view_all_Page );
 
+/* GET detail costume page */
+router.get('/detail', Gadjets_controlers.Gadjets_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', Gadjets_controlers.Gadjets_create_Page);
+
+/* GET create update page */
+router.get('/update', Gadjets_controlers.Gadjets_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', Gadjets_controlers.Gadjets_delete_Page);
+
 module.exports = router;
