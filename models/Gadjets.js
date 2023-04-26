fdf2618fd731c02c1bbb800f124546924d5803de
Neeha-Beerkur <123040403+Neeha-Beerkur@users.noreply.github.com>
2023-04-26 14:45:46 -0500

    Added

--- models/Gadjets.js
+++ models/Gadjets.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const GadjetsSchema = mongoose.Schema({
-Gadjet_Type: String,
-Gadjet_Name: String,
+Gadjet_Type: {type: String, minLength:[4,'itemtype']},
+Gadjet_Name: {type: String, maxLength:[8,'itemname']},
 Gadjet_Price:{type: Number, min:0, max:70000 },
 })
 module.exports = mongoose.model("Gadjets",GadjetsSchema)
--- routes/index.js
+++ routes/index.js
@@ -2,56 +2,73 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var Account = require('../models/account');
+
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Gadjets App', user : req.user });
+  res.render('index', { title: 'Gadjets App', user: req.user });
 });
-router.get('/register', function(req, res) {
-  res.render('register', { title: 'Gadjets App Registration'});
+
+router.get('/register', function (req, res) {
+  res.render('register', { title: 'Gadjets App Registration' });
 });
-router.post('/register', function(req, res) {
-  Account.findOne({ username: req.body.username }).then((err, user) =>{
-   if(err) {
-    return res.render('register', { title: 'Registration',
-          message: 'Registration error', account : req.body.username })
-   }
-   if(user == {} ){
-    return res.render('register', { title: 'Registration',
-          message: 'Existing User', account : req.body.username })
-   }
-   let newAccount = new Account({ username : req.body.username });
-   Account.register(newAccount, req.body.password, function(err, user){
+
+router.post('/register', function (req, res) {
+  Account.findOne({ username: req.body.username }).then(function (err, user) {
     if (err) {
-     return res.render('register', { title: 'Registration',
-           message: 'access error', account : req.body.username })
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Registration error',
+        account: req.body.username
+      });
     }
-    if(!user){
-     return res.render('register',{ title: 'Registration',
-           message: 'access error', account : req.body.username })
+    if (user == {}) {
+      return res.render('register', {
+        title: 'Registration',
+        message: 'Existing User',
+        account: req.body.username
+      });
     }
-    console.log('Sucess, redirect');
-    res.redirect('/');
-    })
-   })
- })
-router.get('/login', function(req, res) {
- res.render('login', { title: 'Gadjets App Login', user : req.user });
-});
-router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.redirect('/');
-});
-router.get('/logout', function(req, res, next) { // use post or delete for better safety
-  req.logout( function(err) {
-  if (err) { return next(err);}
-  res.redirect('/');
+    let newAccount = new Account({ username: req.body.username });
+    Account.register(newAccount, req.body.password, function (err, user) {
+      if (err) {
+        return res.render('register', {
+          title: 'Registration',
+          message: 'access error',
+          account: req.body.username
+        });
+      }
+      if (!user) {
+        return res.render('register', {
+          title: 'Registration',
+          message: 'access error',
+          account: req.body.username
+        });
+      }
+      console.log('Sucess, redirect');
+      res.redirect('/');
+    });
   });
 });
-router.get('/ping', function(req, res){
-  res.status(200).send("pong!");
+
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Gadjets App Login', user: req.user });
 });
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-  if(req.session.returnTo)
-   res.redirect(req.session.returnTo);
+  if (req.session.returnTo) {
+    res.redirect(req.session.returnTo);
+  }
   res.redirect('/');
- });
-module.exports=router;
+});
+
+router.get('/logout', function (req, res, next) { // use post or delete for better safety
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+router.get('/ping', function (req, res) {
+  res.status(200).send("pong!");
+});
+
+module.exports = router;
